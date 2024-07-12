580562f5591f86431866650d69aa1758cfa6ba40
uchida ryutaro <nwr.uchida084@gmail.com>
2024-07-13 08:43:29 +0900

    テーブル追加とシーダーの設定

---
+++ src/database/migrations/2024_07_10_222736_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('restaurant_id')->constrained()->cascadeOnDelete();
+            $table->date('date');
+            $table->time('time');
+            $table->integer('number');
+            $table->boolean('delete_flag')->nullable();
+            $table->timestamps();
+            $table->unique(['user_id', 'restaurant_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
