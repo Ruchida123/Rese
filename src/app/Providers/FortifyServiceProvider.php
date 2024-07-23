63a7d898723f0248d52a2637bcd8d76ca4f7f426
uchida ryutaro <nwr.uchida084@gmail.com>
2024-07-23 23:55:58 +0900

    ログアウト後の遷移先をログイン画面に変更

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -46,7 +46,7 @@ class FortifyServiceProvider extends ServiceProvider
         $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
             public function toResponse($request)
             {
-                return redirect('/');
+                return redirect('/login');
             }
         });
     }
