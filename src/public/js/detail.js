03fcaf7453446138bc1f7994afd2ea3c8bafac80
uchida ryutaro <nwr.uchida084@gmail.com>
2024-08-04 14:44:39 +0900

    予約日時選択処理修正

--- src/public/js/detail.js
+++ src/public/js/detail.js
@@ -10771,6 +10771,7 @@ $(function () {
   $(document).on('change', 'input[class="reserve__date-item"]', function () {
     var val = $(this).val();
     changeText(date, val);
+    changeTimeSelectValue(val);
   });
   var time = $(".reserve__content-time").children('td');
   var timeValue = $('select[class="reserve__time-item"]').val();
@@ -10812,6 +10813,77 @@ $(function () {
   var day = ("00" + dtToday.getDate()).slice(-2);
   var minDate = year + '-' + month + '-' + day;
   $('#reserveDate').attr('min', minDate);
+
+  // 予約時間の選択項目を変更する
+  function changeTimeSelectValue(val) {
+    var dtToday = new Date();
+    var dtSelectVal = new Date(val);
+    // 予約時間テーブル
+    var time_table = [{
+      value: "",
+      label: "予約時間を選択してください"
+    }];
+
+    // 予約時間のセレクトを初期化
+    $('select[class="reserve__time-item"]').children().remove();
+    if (compareDateOnly(dtToday, dtSelectVal)) {
+      var hour = dtToday.getHours();
+      var minute = dtToday.getMinutes();
+      for (var i = hour; i < 24; i++) {
+        if (i == hour) {
+          if (minute < 30) {
+            time_table.push({
+              value: ('0' + i).slice(-2) + ':30',
+              label: ('0' + i).slice(-2) + ':30'
+            });
+          }
+          continue;
+        }
+        time_table.push({
+          value: ('0' + i).slice(-2) + ':00',
+          label: ('0' + i).slice(-2) + ':00'
+        });
+        time_table.push({
+          value: ('0' + i).slice(-2) + ':30',
+          label: ('0' + i).slice(-2) + ':30'
+        });
+      }
+      ;
+    } else {
+      for (var _i = 0; _i < 24; _i++) {
+        time_table.push({
+          value: ('0' + _i).slice(-2) + ':00',
+          label: ('0' + _i).slice(-2) + ':00'
+        });
+        time_table.push({
+          value: ('0' + _i).slice(-2) + ':30',
+          label: ('0' + _i).slice(-2) + ':30'
+        });
+      }
+      ;
+    }
+
+    // 予約時間テーブルをセット
+    time_table.forEach(function (table) {
+      $('select[class="reserve__time-item"]').append($("<option>").attr({
+        value: table.value
+      }).text(table.label));
+    });
+  }
+
+  // 日付が同じかどうか
+  function compareDateOnly(date1, date2) {
+    var year1 = date1.getFullYear();
+    var month1 = date1.getMonth() + 1;
+    var day1 = date1.getDate();
+    var year2 = date2.getFullYear();
+    var month2 = date2.getMonth() + 1;
+    var day2 = date2.getDate();
+    if (year1 == year2 && month1 == month2 && day1 == day2) {
+      return true;
+    }
+    return false;
+  }
 });
 /******/ })()
 ;
